ddb93c04b0fa402b78c309978066369a235fc730
Vinay Thoke <reachme@vinaythoke.com>
2026-02-02 16:14:58 +0530

    fix: resolve storybook addon conflict by removing addon-interactions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/addon-a11y",
     "@storybook/addon-designs",
     "@storybook/experimental-addon-test",
